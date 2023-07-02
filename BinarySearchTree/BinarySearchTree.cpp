caa12694fd886168c2460cff611f44875100fa4d
safirasn21 <safirasn021@gmai.com>
2023-07-02 10:21:43 +0700

    menampilkan output dengan methode switch case

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -115,6 +115,14 @@ int main()
 	BinaryTree obj;
 	while (true)
 	{
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. perform inorder traversal" << endl;
+		cout << "3. perform preorder traversal" << endl;
+		cout << "4. perform postorder traversal" << endl;
+		cout << "5. exit" << endl;
+		cout << "\nEnter your choice (1-5) : " << endl;
+
 		
 	}
 }
