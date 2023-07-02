6b9c6e3e751fb61b4e9be3ddd7860e5afdb0e647
safirasn21 <safirasn021@gmai.com>
2023-07-02 10:20:06 +0700

    melengkapi sintaks traversal postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -102,5 +102,11 @@ public:
 			cout << "Tree is empty" << endl;
 			return;
 		}
+		if (ptr != NULL) {
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << "  ";
+		}
 	}
 };
+
