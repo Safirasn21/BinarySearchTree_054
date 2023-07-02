19bda7e7a662b4bd444d56ae9a44caaa1c77f430
safirasn21 <safirasn021@gmai.com>
2023-07-02 10:03:43 +0700

    menambahkan fungsi insert(menambahkan) dengan parameter string element

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -24,4 +24,12 @@ public:
 		ROOT = NULL; //initializing ROOT to NULL
 	}
 
+	void insert(string element) //insert a node in binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; //Assign value to the data field of the node
+		newNode->leftchild = NULL; //Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; //Make the right child of the new node point to NULL
+
+	}
 };
